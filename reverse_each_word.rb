91877bf52a6f7bd981e0a13a9a2fe33b1446ff03
Matthew McCarthy <mmccarthy64@hotmail.com>
2020-08-21 19:16:30 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
-def reverse_each_word(string)
-  original_array = string.split(" ")
+def reverse_each_word_with_each(array)
+  originalstring = string.split(" ")
   return_array = []
-  original_array.each do |string|
+  originalstring.each do |string|
     return_array << string.reverse
   end
   return_array.join(" ")
